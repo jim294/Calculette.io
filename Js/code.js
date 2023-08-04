7118f40a6a10c4743af825e3c8af00689e097924
Jean Marie DESCHAMPS <134277343+jim294@users.noreply.github.com>
2023-08-04 20:57:53 +0200

    Update code.js

--- Js/code.js
+++ Js/code.js
@@ -22,7 +22,7 @@ function calcul(op,nb1, nb2){
         result=nb1+nb2
     } else if(op=="-"){
         result=nb1-nb2
-    } else if(op=="*"){
+    } else if(op=="x"){
         result=nb1*nb2
     }else if(op=="/"){
         result=nb1/nb2
